5cc0c7e758d174aa07e2dadd08cdb15da64a640e
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-02-27 15:21:07 -0500

    Johnathan Huggett Bredban Meredith, rockets

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-test test test 
+const redBox = document.getElementsByClassName("block block--red");
+const blueBox = document.getElementsByClassName("block block--blue");
+const greenBox = document.getElementsByClassName("block block--green");
+const pinkBox = document.getElementsByClassName("block block--pink");
+const greyBox = document.getElementsByClassName("block block--gray");
+
+
+
