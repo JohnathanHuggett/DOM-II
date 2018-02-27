5fadc2bf508c2caee6611c78328466cabbd2e16a
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-02-27 18:20:48 -0500

    Johnathan Huggett Bredban Meredith, rockets completed

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const redBox = document.getElementsByClassName("block block--red");
-const blueBox = document.getElementsByClassName("block block--blue");
-const greenBox = document.getElementsByClassName("block block--green");
-const pinkBox = document.getElementsByClassName("block block--pink");
-const greyBox = document.getElementsByClassName("block block--gray");
 
+// set variables to all blocks 
+const block = document.querySelectorAll(".block");
+
+
+block.forEach((elements) => {
+    elements.addEventListener("click", () => {
+        const blocks = elements.parentNode; // resign the parent of block which is "blocks"
+        //console.log(blocks); // test to see if parent class is printed
+        blocks.removeChild(elements); // removes 
+        blocks.prepend(elements); // parent.prepend adds element to top of HTML list 
+    });
+});
+
+   
 
 
